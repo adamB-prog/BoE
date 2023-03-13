d08bb79c0be5e4e864a8e557be334383778d803c
Básthy Ádám <basthyadi@gmail.com>
2023-03-13 17:23:30 +0100

    Reorder code

--- Assets/Scripts/AI/NavigationSystem.cs
+++ Assets/Scripts/AI/NavigationSystem.cs
@@ -15,73 +15,57 @@ public class NavigationSystem : MonoBehaviour
     void Start()
     {
         target = new GameObject(string.Format("{0}'s target", gameObject.name));
-        target.transform.position = new Vector3(rnd.NextFloat(-10f, -40f), rnd.NextFloat(-350f, -370f), rnd.NextFloat(-300f,-400f));
-        
+        target.transform.position = new Vector3(rnd.NextFloat(-10f, -40f), rnd.NextFloat(-350f, -370f),
+            rnd.NextFloat(-300f, -400f));
     }
 
     void Update()
     {
         HandleTargetChanging();
         Debug.DrawLine(transform.position, target.transform.position);
-
     }
-    
+
     void HandleTargetChanging()
     {
         Vector3 targetDir = target.transform.position - transform.position;
-        if (targetDir.magnitude <= changeDistance || target is null)
+        if (target is null || targetDir.magnitude <= changeDistance)
         {
             Destroy(target);
             target = new GameObject(string.Format("{0}'s target", gameObject.name));
-            target.transform.position = new Vector3(rnd.NextFloat(-10f, -40f), rnd.NextFloat(-350f, -370f), rnd.NextFloat(-300f,-400f));
+            target.transform.position = new Vector3(rnd.NextFloat(-10f, -40f), rnd.NextFloat(-355f, -365f),
+                rnd.NextFloat(-300f, -400f));
         }
     }
     
-    public float CorrectionAngle(Vector3 axis)
-    {
-        Vector3 targetDir = target.transform.position - transform.position;
-
-        Vector3 forward = transform.forward;
-
-        float angle = Vector3.SignedAngle(targetDir, transform.position + forward, Vector3.up);
-
-
-        return angle;
-    }
 
     public float CorrectionAngleXZ()
     {
         Vector2 targetXZ = new Vector2(target.transform.position.x, target.transform.position.z);
         Vector2 localXZ = new Vector2(transform.position.x, transform.position.z);
-        
+
 
         Vector2 targetDir = targetXZ - localXZ;
 
         Vector2 forward = new Vector2(transform.forward.x, transform.forward.z);
-        
+
         float angle = Vector2.SignedAngle(targetDir, forward);
 
         return angle;
     }
-    
+    //Need to check
     public float CorrectionAngleYZ()
     {
-        
         Vector2 targetYZ = new Vector2(target.transform.position.y, target.transform.position.z);
         Vector2 localYZ = new Vector2(transform.position.y, transform.position.z);
-        
+
 
         Vector2 targetDir = targetYZ - localYZ;
 
         Vector2 forward = new Vector2(transform.forward.y, transform.forward.z);
-        
+
         float angle = Vector2.SignedAngle(targetDir, forward);
 
         return angle;
     }
 
-    
-    
-
-    
 }
